9706f5ef46d6dd24375d2fb47b6b553235f95787
Lukasz Kot <lukasz.kot@hotmail.com>
2021-12-21 09:17:46 +0100

    Using API via axios.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
 
 function App() {
+
+	useEffect(() => {
+		axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+		.then(res => {
+			
+		})
+		.catch(err => console.log(err))
+	})
+
 	return (
 		<div className='App'>
 			<h1>Currency React App</h1>
