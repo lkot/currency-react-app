906b1e8a1911fa6f4b63317749422fa45f25f12c
Lukasz Kot <lukasz.kot@hotmail.com>
2021-12-21 09:35:13 +0100

    Basic markup added.

--- src/App.js
+++ src/App.js
@@ -4,22 +4,32 @@ import React, { useState, useEffect } from 'react';
 
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
-
 function App() {
+	const [coins, setCoins] = useState([]);
 
 	useEffect(() => {
-		axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-		.then(res => {
-			
-		})
-		.catch(err => console.log(err))
-	})
+		axios
+			.get(
+				'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+			)
+			.then((res) => {
+				setCoins(res.data);
+				// console.log(res.data)
+			})
+			.catch((err) => console.log(err));
+	}, []);
 
 	return (
-		<div className='App'>
-			<h1>Currency React App</h1>
-      <p>This is initial commit.</p>
+		<div className='coin-app'>
+			<div className='coin-search'>
+			<h1 className='coin-text'>Search a currency</h1>
+			<form>
+				<input type='text' placeholder='Search' className='coin-input' />
+			</form>
+			</div>
+			
 		</div>
+
 	);
 }
 
