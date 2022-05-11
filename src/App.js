99a12fa8f9f6518ce2f5f48a74c72d482ff62a75
Lukasz Kot <lukasz.kot@hotmail.com>
2022-05-11 11:12:04 +0200

    App structure refactored + API link extracted.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import Coin from './Coin';
+import Coin from './components/Coin/Coin';
 
-// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+const API_LINK =
+	'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
 function App() {
 	const [coins, setCoins] = useState([]);
@@ -11,9 +12,7 @@ function App() {
 
 	useEffect(() => {
 		axios
-			.get(
-				'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
-			)
+			.get(API_LINK)
 			.then((res) => {
 				setCoins(res.data);
 				// console.log(res.data)
