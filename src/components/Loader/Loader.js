57d6be5adc0f08cb21e1ef010d60def2a85626d6
Lukasz Kot <lukasz.kot@hotmail.com>
2022-05-11 11:18:30 +0200

    Loader component's HTML structure implemented.

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import spinner from './spinner.gif';
 
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
-}
+	return (
+		<div className='loader'>
+			<img src={spinner} alt='Loading spinner' />
+			<h1>Fetching data...</h1>
+		</div>
+	);
+};
 
-export default Loader
+export default Loader;
